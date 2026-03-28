16b59a639a6e115755032d7adc941184aa5de112
rahullohchab5210 <rahullohchab5210@gmail.com>
2026-03-28 23:04:06 +0530

    hover:navlinks

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,11 +1,11 @@
 export const SLIDER_DATA = [
     {
-        src: "/Images/webp/sliderImage.webp",
+        src: "/assets/images/webp/sliderImage.webp",
     },
     {
-        src: "/Images/webp/sliderImage.webp",
+        src: "/assets/images/webp/sliderImage.webp",
     },
     {
-        src: "/Images/webp/sliderImage.webp",
+        src: "/assets/images/webp/sliderImage.webp",
     },
 ];
