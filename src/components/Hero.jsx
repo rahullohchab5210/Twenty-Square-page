358662ecaf64c74aaa777f613d7ed44a05bd93ad
rahullohchab5210 <rahullohchab5210@gmail.com>
2026-03-30 14:39:02 +0530

    bg-gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <div className="flex items-start justify-center px-6 pt-39.25 max-[1170px]:pt-25">
       <div className="flex flex-col items-center text-center">
-        <h1 className="font-semibold lg:max-w-182 max-w-150 text-5xl sm:text-6xl lg:text-7xl leading-[121%] tracking-normal bg-gradient-to-r from-[#000000] to-[#494949] bg-clip-text text-transparent">
+        <h1 className="font-semibold lg:max-w-182 max-w-150 text-5xl sm:text-6xl lg:text-7xl leading-[121%] tracking-normal bg-linear-to-r from-[#000000] to-[#494949] bg-clip-text text-transparent">
           A wonderful way to Transform your walls.
         </h1>
         <p className="pt-6.25 pb-6.25 lg:max-w-170 md:max-w-125 max-w-100 font-medium lg:text-xl leading-[140%] text-black">
